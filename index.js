4891fb5626fdf5e7c0935191aacdb9a9a4f6c971
Satyam Lal <satyamlal@users.noreply.github.com>
2025-05-18 00:54:50 +0530

    basic routes created

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { createUserRoutes } = require("./user");
+const { createCourseRoutes } = require("./course");
 
-app.post("/user/signup", (req, res) => {
-  res.json({
-    message: "SignUp endpoint!",
-  });
-});
-
-app.get("/user/signin", (req, res) => {
-  res.json({
-    message: "sign-in endpoint",
-  });
-});
-
-app.get("/user/purchases", (req, res) => {
-  res.json({
-    message: "All my purchased courses!",
-  });
-});
-
-app.post("/course/purchase", (req, res) => {
-  res.json({
-    message: "Buy this course!",
-  });
-});
-
-app.get("/courses", (req, res) => {
-  res.json({
-    message: "All the courses here!",
-  });
-});
+createUserRoutes(app);
+createCourseRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server is listening to Port {port}`);
