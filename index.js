b971739f25d4b9690d975220a4c6959c5839b0fb
Satyam Lal <satyamlal@users.noreply.github.com>
2025-05-21 18:40:48 +0530

    env file added for privacy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: "./config.env" });
+
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -6,7 +8,7 @@ const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
@@ -17,13 +19,18 @@ app.get("/", (req, res) => {
 });
 
 async function main() {
-  await mongoose.connect(
-    "mongodb+srv://ssatyamlal:RKJXGKJVRMok4cCx@cluster0-coursesellinga.sasqe7z.mongodb.net/coursera-app"
-  );
+  try {
+    await mongoose.connect(process.env.MONGO_SERVER);
+    console.log("✅ MongoDB connected");
 
-  app.listen(port, () => {
-    console.log(`Server is listening to Port ${port}`);
-  });
+    app.listen(port, () => {
+      console.log(`Server is listening to Port ${port}`);
+    });
+  } catch (err) {
+    console.log("❌ Database connection failed!");
+    console.log(err);
+    process.exit(1);
+  }
 }
 
 main();
