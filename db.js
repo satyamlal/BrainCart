e88c653204c55abc14f578c56cce1b69a8b3d76b
Satyam Lal <satyamlal@users.noreply.github.com>
2025-05-21 19:26:32 +0530

    Databse updated - comments added

--- db.js
+++ db.js
@@ -22,11 +22,13 @@ const courseSchema = new Schema({
   price: Number,
   imageUrl: String,
   creatorId: ObjectId,
+  //creatorId field refers to an entry in adminSchema. If there is an entry in the adminSchema with some Id(example - Id : 123 ) then only there will be a course here in this Schema with the same objectId (123) in this Schema.
+  //You can't have a creatorId in this Schema that doesn't already exists in the adminSchema, only the admin is able to create any course, so you can't have a random objectId in the Schema for the creatorId.
 });
 
 const purchaseSchema = new Schema({
-  userId: ObjectId,
-  courseId: ObjectId,
+  userId: ObjectId, //userId is pointing to the userSchema
+  courseId: ObjectId, //courseId is pointing to the courseSchema
 });
 
 const userModel = mongoose.model("user", userSchema);
