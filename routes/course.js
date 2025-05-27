81a517f1fe4d641d607f3c8abf5d17620f681698
Satyam Lal <satyamlal@users.noreply.github.com>
2025-05-27 21:40:03 +0530

    routes/courses.js - shows all publically available courses

--- routes/course.js
+++ routes/course.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const courseRouter = Router();
+const { courseModel } = require("./db");
 
 courseRouter.post("/purchase", (req, res) => {
   res.json({
@@ -7,10 +8,21 @@ courseRouter.post("/purchase", (req, res) => {
   });
 });
 
-courseRouter.get("/preview", (req, res) => {
-  res.json({
-    message: "All the course Preview here!",
-  });
+courseRouter.get("/preview", async (req, res) => {
+  try {
+    const courses = await courseModel
+      .find()
+      .select("title description price imageUrl");
+
+    res.status(200).json({
+      message: "All available couses!",
+    });
+  } catch (error) {
+    console.log("Error fetching courses!", error);
+    res.status(500).json({
+      message: "Internal server error in fetching all courses!",
+    });
+  }
 });
 
 module.exports = {
